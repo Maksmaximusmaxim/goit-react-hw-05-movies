66908863c8f90f932e809855c320ca40f1133787
maxim <94103983+Maksmaximusmaxim@users.noreply.github.com>
2022-07-27 09:03:41 +0300

    y

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import { Navigation } from './navigation/navigation';
 import { Home } from './page/home/home';
 import {Films} from './page/films/films';
+import {Erorr} from './page/erorr/erorr'
 export const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/"  element={<Navigation/>}>
           <Route index element={<Home/>}/>
-          <Route  path="/films" element={<Films/>} />
+          <Route  path="films" element={<Films/>} />
+          <Route path='*' element={<Erorr/>}/>
         </Route>
       </Routes>
     </div>
---
+++ src/components/page/erorr/erorr.jsx
@@ -0,0 +1,5 @@
+export function Erorr() {
+    return (
+        <h1>404 страница не найдена (🦆🐁🦝🐇)</h1>
+    )
+}
