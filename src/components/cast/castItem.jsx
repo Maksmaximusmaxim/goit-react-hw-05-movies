5b4e2922adf7f192fe0fc586987097598d6a39a1
maxim <94103983+Maksmaximusmaxim@users.noreply.github.com>
2022-09-03 20:53:11 +0300

    h

--- src/components/cast/castItem.jsx
+++ src/components/cast/castItem.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
+import css from 'components/cast/cast.module.css'
 export function CastItem({ img, character, name }) {
   return (
     <ListItem alignItems="flex-start">
@@ -10,7 +11,7 @@ export function CastItem({ img, character, name }) {
        </ListItemAvatar>
       
       <h2>{name}</h2>
-      <h3>{character}</h3>
+      <h3 className={css.name} >{character}</h3>
     </ListItem>
   );
 }
